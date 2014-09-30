ab84e1360f8225e8c5eb0c7a396fedbd609a4da8
Sigurbjorn Kristjansson <sigurbjornk@gmail.com>
2014-09-30 19:23:01 +0000

    problem 4, files created

--- Ruber2/src/java/is/ru/honn/ruber/process/TripImportProcess.java
+++ Ruber2/src/java/is/ru/honn/ruber/process/TripImportProcess.java
@@ -1,7 +1,26 @@
 package is.ru.honn.ruber.process;
 
+import is.ru.honn.ruber.domain.TripHandler;
+import is.ruframework.process.RuAbstractProcess;
+
 /**
  * Created by sveinn on 9/30/14.
  */
-public class TripImportProcess {
+public class TripImportProcess extends RuAbstractProcess implements TripHandler
+{
+
+    public void beforeProcess()
+    {
+        System.out.println("beforeProcess");
+    }
+
+    public void startProcess()
+    {
+        System.out.println("startProcess");
+    }
+
+    public void afterProcess()
+    {
+        System.out.println("afterProcess");
+    }
 }
